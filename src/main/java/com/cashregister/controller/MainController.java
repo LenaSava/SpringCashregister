70c528bb9a3ca0499e8ca06eebd71ca3c194e49d
student_Savinkova <owlindacity@gmail.com>
2019-08-08 18:39:14 +0300

    authorization

--- src/main/java/com/cashregister/controller/MainController.java
+++ src/main/java/com/cashregister/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.cashregister;
+package com.cashregister.controller;
 
 import com.cashregister.domain.Message;
 import com.cashregister.repos.MessageRepos;
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepos messageRepos;
 
---
+++ src/main/java/com/cashregister/domain/Role.java
@@ -0,0 +1,5 @@
+package com.cashregister.domain;
+
+public enum Role {
+    USER;
+}
