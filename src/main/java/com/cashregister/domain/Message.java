e84750d65974bad3f406aa85533d2be4e4f152a7
student_Savinkova <owlindacity@gmail.com>
2019-08-08 16:35:02 +0300

    before authorization

---
+++ src/main/java/com/cashregister/domain/Message.java
@@ -0,0 +1,41 @@
+package com.cashregister.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.criteria.CriteriaBuilder;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String text;
+    private String tag;
+
+    public Message() {
+    }
+
+    public Message(String text, String tag) {
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+}
