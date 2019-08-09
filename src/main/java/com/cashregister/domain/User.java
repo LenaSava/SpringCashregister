8e77fdd9d537e89e3f94c76346a7c2bddd20df9e
student_Savinkova <owlindacity@gmail.com>
2019-08-09 16:31:23 +0300

    add lombok

--- src/main/java/com/cashregister/domain/User.java
+++ src/main/java/com/cashregister/domain/User.java
@@ -1,8 +1,11 @@
 package com.cashregister.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.Set;
 
+@Data
 @Entity
 @Table(name = "usr")
 public class User {
@@ -18,43 +21,4 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
 }
